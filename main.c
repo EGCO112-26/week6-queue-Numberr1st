9bfc22966666109d7b4a9410c3417d2c84a23a15
Numberr1st <88944342+Numberr1st@users.noreply.github.com>
2026-02-11 08:30:24 +0000

    Assg.6 Queue

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Node.h"
+//#include "Node.h"
 
-//#include "Queue.h"
+#include "Queue.h"
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
